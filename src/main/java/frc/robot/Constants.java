9d3732a7a489b8d77f4a92b45e811767fd6d15d9
Sawyer <58192471+DiMister@users.noreply.github.com>
2022-12-20 08:07:05 -0600

    constants comments

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -8,14 +8,17 @@ public final class Constants {
 
     public static final class ModuleConstants {
         public static final double kWheelDiameterMeters = Units.inchesToMeters(3.94);
+        //gear ratio is inversed (1/gear ratio) so we can multiply instead of dividing the Rot to meter funtion 
         public static final double kDriveMotorGearRatio = 1/8.14;
         public static final double kTurningMotorGearRatio = 1/12.8;
         //2048 is pulses per rotation of the motor
         public static final double kDriveEncoderRot2Meter = kDriveMotorGearRatio * Math.PI * kWheelDiameterMeters*(1.0/2048.0);
         public static final double kTurningEncoderRot2Rad = kTurningMotorGearRatio * 2 * Math.PI*(1.0/2048.0);
         
+        //CTRE mesures their velcity in 100ms, so we multiply it by 10 to get 1s
         public static final double kDriveEncoderRPMS2MeterPerSec = kDriveEncoderRot2Meter*10;
         public static final double kTurningEncoderRPMS2RadPerSec = kTurningEncoderRot2Rad*10;
+        //use guess and check to find. when the module is overshooting this needs to be fine tuned
         public static final double kPTurning = 0.5;
     }
 
@@ -63,14 +66,17 @@ public final class Constants {
         public static final boolean kFrontRightDriveAbsoluteEncoderReversed = false;
         public static final boolean kBackRightDriveAbsoluteEncoderReversed = false;
 
+        //To find set the motors forward record the value
         public static final double kFrontLeftDriveAbsoluteEncoderOffsetRad = 2.1;
         public static final double kBackLeftDriveAbsoluteEncoderOffsetRad = 0;
         public static final double kFrontRightDriveAbsoluteEncoderOffsetRad = 0;
         public static final double kBackRightDriveAbsoluteEncoderOffsetRad = 0;
 
+        //To find set the modules to 100% and see what speed cap out at
         public static final double kPhysicalMaxSpeedMetersPerSecond = 4;
         public static final double kPhysicalMaxAngularSpeedRadiansPerSecond = 7 * 2 * Math.PI;
 
+        //arbitrary chosen based on what drivers pick
         public static final double kTeleDriveMaxSpeedMetersPerSecond = kPhysicalMaxSpeedMetersPerSecond / 4;
         public static final double kTeleDriveMaxAngularSpeedRadiansPerSecond = //
                 kPhysicalMaxAngularSpeedRadiansPerSecond / 4;
@@ -79,17 +85,21 @@ public final class Constants {
     }
 
     public static final class OIConstants {
+        //joystick id
         public static final int kDriverControllerPort = 0;
 
+        //axis and buttons id for drive joysticks
         public static final int kDriverYAxis = 1;
         public static final int kDriverXAxis = 0;
         public static final int kDriverRotAxis = 2;
         public static final int kDriverFieldOrientedButtonIdx = 1;
 
+        //area were joysticks will not activate
         public static final double kDeadband = 0.05;
     }
 
     public static final class SensorConstants {
+        //make a bettter id. 0 is bad
         public static final int kPigeonID = 0;
     }
 }
