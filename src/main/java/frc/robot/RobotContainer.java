edc55e9dbb51e7fa0b3c08e71c481b342773a7dd
606511 <58192471+DiMister@users.noreply.github.com>
2022-12-31 17:08:16 -0600

    yrs

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -24,13 +24,16 @@ import frc.robot.subsystems.SwerveSubsystem;
 
 public class RobotContainer {
         
+        //(Max)create objects of subsytems
     private final SwerveSubsystem swerveSubsystem = new SwerveSubsystem();
 
+        //TODO (Max) Why the hell are they called OIConstants?
     private final Joystick driverJoytick = new Joystick(OIConstants.kDriverControllerPort);
 
     public RobotContainer() {
         //sets up the defalt command for the swerve subsystem. Defalut commands run if no other commands are set
         //the () -> are lambda expressions.
+        //(Max) so the lambda is sending over a method with preset conditions
         swerveSubsystem.setDefaultCommand(new SwerveJoystickCmd(
                 swerveSubsystem,
                 () -> driverJoytick.getRawAxis(OIConstants.kDriverYAxis),
@@ -38,11 +41,15 @@ public class RobotContainer {
                 () -> -driverJoytick.getRawAxis(OIConstants.kDriverRotAxis),
                 () -> !driverJoytick.getRawButton(OIConstants.kDriverFieldOrientedButtonIdx)));
 
+        //TODO (Max) Wait so the buttons will be set up always even in autonmous?
         configureButtonBindings();
     }
 
     private void configureButtonBindings() {
         //reseting button for IMU. Usefull for change field orentation forward direction
+        //(Max) What the hell is creating the ghost buttonNumber?
+        //(Max) Guess buttons are now class
+        //(Max) What is the point of Lambda if it's paramenter is blank does it turn it into a command?
         new JoystickButton(driverJoytick, 5).whenPressed(() -> swerveSubsystem.zeroHeading());
         new JoystickButton(driverJoytick, 3).whenPressed(() -> swerveSubsystem.resetOdometry(new Pose2d()));
     }
@@ -50,23 +57,27 @@ public class RobotContainer {
     //generates a path via points
     public Command getAutonomousCommand() {
         // 1. Create trajectory settings
+        //TODO (Max) GET RID OF THE K IN FRONT OF EVERY CONSTANT
         TrajectoryConfig trajectoryConfig = new TrajectoryConfig(
                 AutoConstants.kMaxSpeedMetersPerSecond,
                 AutoConstants.kMaxAccelerationMetersPerSecondSquared)
                         .setKinematics(DriveConstants.kDriveKinematics);
 
         // 2. Generate trajectory
+        //(Max) why do ghost parameter names only show sometimes?
+        //(Max) Genrates trajectory need to feed start point, a sereris of inbtween points, and end point
+        //(Max) Midpoints it does what angle it wants for you
+        //TODO (Max) shouldn't the start point angle be the current angle of robot
         Trajectory trajectory = TrajectoryGenerator.generateTrajectory(
                 new Pose2d(0, 0, new Rotation2d(0)),
-                List.of(
-                        new Translation2d(1, 0),
-                        new Translation2d(1, -1)),
+                List.of(new Translation2d(1, 0), new Translation2d(1, -1)),
                 new Pose2d(2, -1, Rotation2d.fromDegrees(180)),
                 trajectoryConfig);
 
         // 3. Define PID controllers for tracking trajectory
         PIDController xController = new PIDController(AutoConstants.kPXController, 0, 0);
         PIDController yController = new PIDController(AutoConstants.kPYController, 0, 0);
+        //(Max) what is a PIDController and thetaController is a bad var name
         ProfiledPIDController thetaController = new ProfiledPIDController(
                 AutoConstants.kPThetaController, 0, 0, AutoConstants.kThetaControllerConstraints);
         thetaController.enableContinuousInput(-Math.PI, Math.PI);
@@ -74,6 +85,7 @@ public class RobotContainer {
         // 4. Construct command to follow trajectory
         SwerveControllerCommand swerveControllerCommand = new SwerveControllerCommand(
                 trajectory,
+                //(Max) why is swerveSubsystem::getPose not swerveSubsystem.getPose()?
                 swerveSubsystem::getPose,
                 DriveConstants.kDriveKinematics,
                 xController,
@@ -83,6 +95,7 @@ public class RobotContainer {
                 swerveSubsystem);
 
         // 5. Add some init and wrap-up, and return everything
+        //(Max) creates a Command list that will reset the Odometry to the set start point, then move the path, then stop
         return new SequentialCommandGroup(
                 new InstantCommand(() -> swerveSubsystem.resetOdometry(trajectory.getInitialPose())),
                 swerveControllerCommand,
