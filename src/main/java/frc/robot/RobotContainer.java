49dba8ae9892dc0a1b2dddee0885a7265fe0a84e
ModernTinTin1 <sawyer.c.theis@gmail.com>
2023-01-19 15:35:42 -0600

    modern commands

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -47,8 +47,8 @@ public class RobotContainer {
         //this buttons sould only only muniplate parts that don't move robot
         
         //reseting button for IMU. Usefull for change field orentation forward direction
-        new JoystickButton(driverJoytick, 1).whenPressed(() -> swerveSubsystem.zeroHeading());
-        new JoystickButton(driverJoytick, 2).whenPressed(() -> swerveSubsystem.resetOdometry(new Pose2d()));
+        new JoystickButton(driverJoytick, 1).onTrue(new InstantCommand(() -> swerveSubsystem.zeroHeading()));
+        new JoystickButton(driverJoytick, 2).onTrue(new InstantCommand(() -> swerveSubsystem.resetOdometry(new Pose2d())));
     }
 
     //generates a path via points
