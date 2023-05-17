82e3a114c5dde8eec575d55f9e4ddb0543ff4d03
Sawyer <sawyer.c.theis@gmail.com>
2023-05-17 17:21:32 -0500

    death to the rabbit that walks hallowed halls

--- src/main/java/frc/robot/commands/SwerveDriveToPointCmd.java
+++ src/main/java/frc/robot/commands/SwerveDriveToPointCmd.java
@@ -22,12 +22,7 @@ public class SwerveDriveToPointCmd extends CommandBase {
   }
 
   public SwerveDriveToPointCmd(SwerveSubsystem m_SwerveSubsystem, PosPose2d targetPosition) {
-    this.swerveSubsystem = m_SwerveSubsystem;
-    this.targetPosition = targetPosition.toDrivePose2d();
-    
-    timer = new Timer();
-
-    addRequirements(m_SwerveSubsystem);
+    this(m_SwerveSubsystem, targetPosition.toDrivePose2d());
   }
 
   @Override
